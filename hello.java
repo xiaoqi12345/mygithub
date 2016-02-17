c4bcc8d3c763deaa33c8d75c5151a0d745d9f1f7
mygit <mygit@163.com>
2016-02-17 09:45:53 +0800

    Update hello.java file:add two lines

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello{
-	public static void main(String args[]){
-		System.out.println("hello world£¡¡°£©
-		}
+	public static void main(String[] args){
+		System.out.println("hello world!");
 	}
+}
