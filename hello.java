13b8bfc3b318dce584570f62b1deb0ff4be59609
mygit <mygit@163.com>
2016-02-22 14:22:34 +0800

    add one line on hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("hello world!");
+		
 		System.out.println("add two lines 1");
-		System.out.println("add two lines 2");
-	}
+		
 }
