7d0cd729c1322ab84e62eff0864e0ff19ce0f0cb
mygit <mygit@163.com>
2016-02-23 10:20:17 +0800

    patch apply

--- emp.java
+++ emp.java
@@ -1,6 +1,11 @@
-import jav.io.*;
+import java.io.*;
+import java.util.*;
 
 public class emp implements Serializable{
 	private Integer empno;
 	private String ename;
+	private Date hiredate;
+	private Double sal;
+	private Double comm;
+	private emp mgr;
 }
